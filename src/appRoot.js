7eb855e7ff1572a88fd5cec1d85f17f62404c15e
vitaliy <vitaliy@backbase.com>
2019-12-03 21:27:15 +0200

    Initial commit

---
+++ src/appRoot.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AppRoot = () =>
+  <div className="admin">
+    2020
+  </div>;
+
+export default AppRoot;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client.js',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+
+};

9e15edf8b4f03fa46a1b2f6e7bf8611a798bc361
vitaliy <vitaliy@backbase.com>
2019-12-04 17:50:01 +0200

    add navigation

--- src/appRoot.js
+++ src/appRoot.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-const App = () =>
-  <div className="admin">
-    2020
-  </div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeMenuItem: 'home' };
+  }
+
+  setHomeNavItemActive = () => {
+    this.setState({ activeMenuItem: 'home' });
+  }
+
+  setAdminNavItemActive = () => {
+    this.setState({ activeMenuItem: 'admin' });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav">
+              <li className={"nav-item " + (this.state.activeMenuItem === 'home' && 'active')} data-name="home" onClick={this.setHomeNavItemActive}>
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className={"nav-item " + (this.state.activeMenuItem === 'admin' && 'active')} data-name="admin" onClick={this.setAdminNavItemActive}>
+                <Link className="nav-link" to="/admin">Admin</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/admin">
+              <Admin />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Admin() {
+  return <h2>Admin</h2>;
+}
 
 export default App;
